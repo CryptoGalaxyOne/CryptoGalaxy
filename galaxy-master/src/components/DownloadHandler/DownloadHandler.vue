9f5f01e5314974ec0f53dede773f54322a9ac196
yudazhi <yudz163@163.com>
2018-12-07 10:15:49 +0800

    2018-12-7  update  v1.6.2

--- galaxy-master/src/components/DownloadHandler/DownloadHandler.vue
+++ galaxy-master/src/components/DownloadHandler/DownloadHandler.vue
@@ -4,7 +4,7 @@
     <div class="hd">
       <img class="logo" src="@/assets/img/download_logo.png" alt="" />
       <div class="title">CryptoGalaxy</div>
-      <div class="version">Beta 1.6.1</div>
+      <div class="version">Beta 1.6.2</div>
       <div class="des">Discover &amp; Collect Your Planet </div>
       <div class="subdes">The first ever virtual universe based on blockchain</div>
     </div>
