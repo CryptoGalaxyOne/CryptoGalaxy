23fc2ef4587ffc74a5bf949a933f10f6da2dc163
xyxu <37847940@qq.com>
2019-03-08 18:03:49 +0800

    add cdn

--- galaxy-master/src/components/HomeHandler/modules/banner.vue
+++ galaxy-master/src/components/HomeHandler/modules/banner.vue
@@ -33,8 +33,10 @@
         </div>
         <div class="links" v-if="timesup === 1">
           <a href="https://cryptogalaxy.one/#/ios" class="download-ios">{{$t("banner.ios")}}</a>
+          <!--
           <a href="https://play.google.com/store/apps/details?id=com.zeepin.galaxy" target="_blank" class="download-android">{{$t("banner.googlePlay")}}</a>
-          <a href="https://cryptogalaxy.one/static/CryptoGalaxy2.0.0.apk" class="download-android">{{$t("banner.android")}}</a>
+          -->
+          <a href="http://cdn.zeepin.io/cryptogalaxy/CryptoGalaxy2.0.apk" class="download-android">{{$t("banner.android")}}</a>
         </div>
       </div>
       <img class="vision" src="@/assets/img/vision.png" alt="">
